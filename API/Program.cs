3213e420839c775c46ddcb5a76ae84ae6dcab668
Ezz Saeed <ezzsaid299@gmail.com>
2025-02-04 07:13:13 -0800

    Configuring db context with sql server db provider and identity

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,9 @@
 
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Program
@@ -13,6 +18,12 @@ namespace API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("connection"));
+            });
 
             var app = builder.Build();
 
